f3a4621b84726be837e4720927b565105960e885
Selin Huang <selin.hyx@gmail.com>
2019-07-05 12:12:04 +0000

    used promise to print pass times

---
+++ index2.js
@@ -0,0 +1,21 @@
+ const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require("./iss_promised"); 
+
+//  fetchMyIP()
+//     .then(fetchCoordsByIP)
+//     .then(fetchISSFlyOverTimes)
+//     .then(body => console.log(body));
+
+const timeSchedule = function(passTime) {
+  for (let obj of passTime) {
+    let date = new Date(obj.risetime);
+    console.log(`Next pass at ${date} for ${obj.duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation()
+    .then((passTimes) => {
+      timeSchedule(passTimes);
+    })
+    .catch((error) => {
+      console.log("It didn't work: ", error.message);
+    });
